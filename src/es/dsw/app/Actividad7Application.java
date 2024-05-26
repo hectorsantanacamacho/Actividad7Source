41966f0768015b4f2ba292b2b88a31eacaab3dbc
‘alumno-ets’ <alumno-ets@alumno.ieselrincon.es>
2024-05-26 19:16:18 +0100

    Modificación 1

--- src/es/dsw/app/Actividad7Application.java
+++ src/es/dsw/app/Actividad7Application.java
@@ -1,5 +1,7 @@
 package es.dsw.app;
 
+import es.dsw.models.Profesor;
+
 public class Actividad7Application {
 
 	public static void main(String[] args) {
@@ -12,9 +14,8 @@ public class Actividad7Application {
 		System.out.println("########                                 #########");
 		System.out.println("##################################################");
 		
-		
-
-
+		Profesor objProfesor = new Profesor("Pepito", "Pérez", "Sanchez", "44332234L");
+		System.out.println("El nuevo profesor se llama " + objProfesor.getNombre());
 
 
 		
